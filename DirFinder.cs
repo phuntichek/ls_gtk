3031e2fc992fc31614c182dddb82282011f8db15
Павел <phuntik@MacBook-Air-MacBook.local>
2020-12-18 02:12:47 +0300

    first commit

---
+++ DirFinder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Gtk;
+
+namespace gtk_ls
+{
+
+
+    public class DirFinder
+    {
+        static public Thread currentThread;
+
+        string dir_path;
+        string dir_name;
+        string[] files = new string[0];
+        string[] subdirs = new string[0];
+        TreeIter treeIter;
+        List<DirFinder> subDirFinders = new List<DirFinder>();
+
+        public DirFinder(string parentDir, string name, TreeIter parentTreeIter)
+        {
+            dir_path = parentDir + "/" + name;
+            dir_name = name;
+            treeIter = MainClass.store.AppendValues(parentTreeIter, dir_name);
+        }
+
+        public void fillThreaded()
+        {
+            ThreadStart starter = new ThreadStart(this.fill);
+            starter += () => {
+                // Do what you want in the callback
+                onFillComplete();
+            };
+            currentThread = new Thread(starter) { IsBackground = true };
+            currentThread.Start();
+        }
+
+        public void fill()
+        {
+            
+
+            try
+            {
+                files = Directory.GetFiles(dir_path, MainClass.regex);
+            }
+            catch (Exception e)
+            {
+
+            }
+            try
+            {
+                subdirs = Directory.GetDirectories(dir_path);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            if (subdirs != null)
+            {
+                for (int i = 0; i < subdirs.Length; i++)
+                {
+                    subdirs[i] = subdirs[i].Substring(dir_path.Length + 1);
+                }
+                foreach (string dir in subdirs)
+                {
+                    subDirFinders.Add(new DirFinder(dir_path, dir, treeIter));
+                }
+            }
+            if (files != null)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    files[i] = files[i].Substring(dir_path.Length + 1);
+                    // call GUI to add file
+                    //MainClass.store.AppendValues(treeIter, files[i]);
+                }
+            }
+        }
+
+        //public void fillRecursive(string regex)
+        //{
+        //    fill(regex);
+        //    foreach (DirFinder df in subDirFinders)
+        //    {
+        //        df.fillRecursive(regex);
+        //    }
+        //}
+
+        public void onFillComplete()
+        {
+            if (files != null)
+            {
+                foreach (string file in files)
+                {
+                    MainClass.store.AppendValues(treeIter, file);
+                }
+            }
+            foreach (DirFinder df in subDirFinders)
+            {
+                df.fillThreaded();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using Gtk;
+
+
+namespace gtk_ls
+{
+    
+    class MainClass
+    {
+        static public TreeStore store;
+        static public Window window;
+        static public string regex = "*";
+
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            Window window = new Window("Editor Window");
+
+            window.SetPosition(WindowPosition.Center);
+            window.HeightRequest = 800;
+            window.WidthRequest = 1200;
+
+            TreeView view = new TreeView();
+
+            view.WidthRequest = 500;
+
+            TreeViewColumn column = new TreeViewColumn();
+            column.Title = "Heirarchy";
+
+            CellRendererText cell = new CellRendererText();
+
+            column.PackStart(cell, true);
+
+            view.AppendColumn(column);
+
+            column.AddAttribute(cell, "text", 0);
+
+            store = new TreeStore(typeof(string));
+            TreeIter treeIter = store.AppendValues("SearchResult");
+            DirFinder mainDF = new DirFinder("/Users/phuntik/Projects/", "net_regex_ls", treeIter);
+
+            //on Button Search click
+            mainDF.fillThreaded();
+
+            //on Button Stop click
+            // DirFinder.currentThread.Abort()
+
+            view.ShowExpanders = true;
+
+            view.Model = store;
+            view.ShowExpanders = true;
+
+            window.Add(view);
+
+            //window.DeleteEvent += ExitWindow;
+
+            window.ShowAll();
+
+            Application.Run();
+        }
+    }
+}
